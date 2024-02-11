5686bd67659aebcbd3ef11c43e6216dc1359e776
Z004M0DX <sanjana.jada@siemens.com>
2024-02-11 09:19:13 +0530

    next commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,45 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
-};
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "prettier"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs,vite.config.ts}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react",
+        "prettier",
+        "react-refresh"
+    ],
+    "rules": {
+        "react/react-in-jsx-scope":"off",
+        "prettier/prettier":"error",
+    },
+    settings: {
+        react:{
+            version:"detect",
+        },
+    },
+}
